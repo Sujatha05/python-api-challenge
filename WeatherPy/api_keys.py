6ded5a621640a9b2d91523640a9a88e780722e79
Sujatha05 <jayaraman.sujatha@gmail.com>
2023-10-25 15:17:21 +1100

    added files

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "7296009c25e7d7d9e8fca9b7d2fc9291"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "baf303585bdd4b1baa8e05556de7d75f"
